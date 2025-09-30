3c523368ca05752675539c6af461c96947269521
reden aquino <redengetjar28gmail.com@redens-MacBook-Air.local>
2025-10-01 00:32:00 +0800

    chore(terraform): Added .gitignore to ignore unnecessary files from pushing to repository feat(terraform):   Added new aws provider that assumes the provisioner role   Added S3 backend for tfstate location as well as dynamodb table for tfstatelock   Specified terraform workspace key prefix and tfstate key   Added required provider aws plugin

---
+++ terraform/env/dev/providers.tf
@@ -0,0 +1,29 @@
+terraform {
+    backend "s3" {
+        bucket  = "event-logger-terraform-state"
+        region  = "ap-southeast-1"
+        workspace_key_prefix = "env"
+        key = "event-logger.tfstate"
+
+        dynamodb_table  = "event-logger-terraform-state-locks"
+        encrypt = true
+    }
+
+    required_version = ">= 1.5.0"
+
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = ">= 5.0.0"
+        }
+    }
+
+}
+
+provider "aws" {
+    region = "ap-southeast-1"
+
+    assume_role {
+      role_arn = "arn:aws:iam::297416720645:role/event-logger-provisioner-role"
+    }  
+}
