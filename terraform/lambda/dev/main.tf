57d9a91135b18561611fb2c2ed8bde87759c122b
reden aquino <redengetjar28gmail.com@redens-MacBook-Air.local>
2025-10-02 04:06:26 +0800

    feat(terraform): Added permission for lambda to access dynamoDB feat(ci): Used short commit hash as IMAGE_TAG to make sure that every change in docker image will be deployed feat(aws): Update permission in provisioner policy refactor(app): Deleted event-logger-api.py as will no longer be needed

--- terraform/lambda/dev/main.tf
+++ terraform/lambda/dev/main.tf
@@ -34,8 +34,46 @@ resource "aws_lambda_function" "event_logger" {
   package_type  = "Image"
   image_uri     = "${var.ecr_repository_uri}:${var.image_tag}"
 
+  publish       = true
+
   memory_size   = 128
   timeout       = 15
 
   tags = local.required_tags
+}
+
+resource "aws_lambda_alias" "event_logger" {
+  name = "${local.prefix}-alias"
+  function_name    = aws_lambda_function.event_logger.function_name
+  function_version = aws_lambda_function.event_logger.version
+}
+
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "${local.prefix}-lambda-execution-policy"
+  role = aws_iam_role.lambda_execution_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:PutItem",
+          "dynamodb:GetItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:DeleteItem"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
